1c41086773079e95fb5910d209764f80dee117c2
Replit user <>
2023-05-08 04:04:08 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -125,7 +125,7 @@ Pair * firstMap(HashMap * map) {
 }
 
 Pair * nextMap(HashMap * map) {
-  if(map->current=-1) return NULL;
+  if(map->current==NULL) return NULL;
   for(int i = map->current+1; i < map->capacity; i++){
     if (*(map->buckets+i) != NULL && (*(map->buckets+i))->key != NULL) { 
     map->current = i;
